cf2220537c1b8d4f5f03003db1e28ddb144a0b59
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-01-12 09:03:45 +0700

    add writeOnlyClass exercise

---
+++ src/writeOnlyClass/Student.java
@@ -0,0 +1,21 @@
+package writeOnlyClass;
+
+public class Student {
+
+    private String name;
+    private String classes;
+
+    public Student()
+    {
+        this.name = "John";
+        this.classes = "C02";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/writeOnlyClass/Test.java
@@ -0,0 +1,10 @@
+package writeOnlyClass;
+
+public class Test {
+    public static void main(String[] args) {
+         Student student = new Student();
+         student.setName("Nguyen");
+         student.setClasses("C01");
+
+    }
+}
