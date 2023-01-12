6e7028e25ab32c8dcb933da013ab4ec05732f6e0
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-01-12 16:35:17 +0700

    add point3D exercise

---
+++ src/point3D/Main.java
@@ -0,0 +1,11 @@
+package point3D;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(5,8);
+        System.out.println("Tọa độ 2D là : " +point2D);
+        Point3D point3D = new Point3D(5,10,8);
+        System.out.println("Tọa độ 3D là : " +point3D);
+    }
+}
