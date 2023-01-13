266763e65eac744270630b1c6a740d662b3aa4ba
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-01-13 11:53:48 +0700

    add exercise shape

--- src/shape/Square.java
+++ src/shape/Square.java
@@ -1,2 +1,46 @@
-package shape;public class Square {
+package shape;
+
+public class Square extends Rectangle
+{
+    public Square()
+    {
+        super();
+    }
+
+    public Square(double side, String color, boolean filled)
+    {
+       super(color, filled, side,side);
+    }
+
+    public Square(double side)
+    {
+       super(side,side);
+    }
+    public double getSide()
+    {
+        return super.getWidth();
+    }
+    public void setSide(double side)
+    {
+         this.setWidth(side);
+         this.setHeight(side);
+    }
+
+    // set up private square width va height
+    @Override
+    public void setWidth(double width)
+    {
+        this.setSide(width);
+    }
+
+    @Override
+    public void setHeight(double height)
+    {
+        this.setSide(height);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="+this.getSide()+", which is a subclass of "+super.toString()+" ";
+    }
 }
